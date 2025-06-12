e283dc892ee601bbfb31d3b9b69115074e379af5
kanggyeonggu <rudrn0110@naver.com>
2025-06-12 16:19:19 +0900

    60. 프로토 타입 스코프 - 싱글톤 빈과 함께 사용 시 Provider로 문제 해결

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -4,9 +4,12 @@ import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
+
+import jakarta.inject.Provider;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -35,20 +38,58 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
 
     }
 
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean; // 생성 시점에 주입 완료
+//        private final PrototypeBean prototypeBean; // 생성 시점에 주입 완료
+
+//        @Autowired
+//        public ClientBean(PrototypeBean prototypeBean) {
+//            this.prototypeBean = prototypeBean;
+//        }
+
+
+        /**
+         * PrototypeBean.init -> hello.core.scope.SingletonWithPrototypeTest1$PrototypeBean@59e32960
+         * PrototypeBean.init -> hello.core.scope.SingletonWithPrototypeTest1$PrototypeBean@539d019
+         * prototypeBeanProvider.getObject();
+         * => getObject() 호출 시 스프링 컨테이너에서 해당하는 빈을 찾아서 제공해준다. (DL, Dependency Lookup)
+         * => applicationContext 에서 직접 찾지 않아도 된다.
+         * => 필요할 때마다 스프링 컨테이너에 요청하는 기능이다.
+         * ==> 스프링이 제공하는 기능을 사용하지만, 기능이 단순하므로 단위테스트 작성 또는 Mock 코드 작성이 훨씬 쉬워진다.
+         * ==> ObjectProvider 는 지금 딱 필요한 DL 정도 기능만 제공한다.
+         * ===> 하지만 결국 '스프링에 의존'한다.
+         * */
+//        private final ObjectProvider<PrototypeBean> prototypeBeanProvider;
+
+//        public ClientBean(ObjectProvider<PrototypeBean> prototypeBeanProvider) {
+//            this.prototypeBeanProvider = prototypeBeanProvider;
+//        }
+
+//        public int logic() {
+//            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+//            prototypeBean.addCount();
+//            return prototypeBean.getCount();
+//        }
+
+
+        /**
+         * javax -> jakarta
+         * Provider?
+         *
+         * */
+        private final Provider<PrototypeBean> prototypeBeanProvider;
 
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
+        public ClientBean(Provider<PrototypeBean> prototypeBeanProvider) {
+            this.prototypeBeanProvider = prototypeBeanProvider;
         }
 
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
